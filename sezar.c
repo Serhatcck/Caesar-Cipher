b79b38b1fc10c51d3f99919bb7a2abfd97b1fbd0
Serhat ÇİÇEK <49496846+Serhatcck@users.noreply.github.com>
2019-04-26 03:22:21 +0300

    Add files via upload

---
+++ sezar.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void shift(char *word, int key){
+     char c = word[0];
+     int i = 0;
+     while(c!='\0'){
+       if(c>=65&&c<=90){
+                        word[i]=(word[i]+key);
+                        if(word[i]>90)
+                                       word[i]= (word[i]%90)+65;
+       }
+       if(c>=97&&c<=122){
+                        word[i]=(word[i]+key);
+                        if(word[i]>122)
+                                       word[i]= (word[i]%122)+97;
+       }
+       i++;
+       c=word[i];
+     }
+
+}
+int main(){
+    char word[100]="Hello World";
+    printf("Pleas enter key");
+    int key;
+    scanf("%d",&key);
+    shift(word,key);
+    printf("encoded :%s\n",word);
+    shift(word,-2);
+    printf("unencoded:%s",word);
+}
